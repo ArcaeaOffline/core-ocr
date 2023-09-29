da42699ac823da83d34d45ee46c41a529e2b8af8
283375 <bilibili283375@163.com>
2023-09-29 21:28:43 +0800

    refactor: auto sizes

---
+++ src/arcaea_offline_ocr/extractor/sizes/auto/__init__.py
@@ -0,0 +1,3 @@
+from .common import AutoSizes
+from .t1 import AutoSizesT1
+from .t2 import AutoSizesT2
---
+++ src/arcaea_offline_ocr/extractor/sizes/auto/common.py
@@ -0,0 +1,7 @@
+from ..common import Sizes
+
+
+class AutoSizes(Sizes):
+    def __init__(self, w: int, h: int):
+        self.w = w
+        self.h = h
